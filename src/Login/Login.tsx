74dc6d3fb13ffe237d1d80af548f748ee6b24379
Evelin <szabadosevelin7@gmail.com>
2020-09-25 10:57:59 +0200

    Add basic form to login component

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,11 +1,57 @@
-import { removeEmitHelper } from "typescript";
 
 import React from 'react'
+import { Form, Input, Button, Checkbox } from 'antd';
+
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+};
+const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+};
 
 export default function Login() {
+    const onFinish = (values: string) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
     return (
-        <div>
-            Login
-        </div>
+        <Form
+            {...layout}
+            name="basic"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+        >
+            <Form.Item
+                label="Email"
+                name="email"
+
+                rules={[{ required: true, message: 'Please enter your email!' }]}
+            >
+                <Input type="email" />
+            </Form.Item>
+
+            <Form.Item
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: 'Please enter your password!' }]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item {...tailLayout}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+          </Button>
+            </Form.Item>
+        </Form>
     )
 }
