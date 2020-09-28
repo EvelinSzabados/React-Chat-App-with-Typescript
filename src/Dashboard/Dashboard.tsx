65eb292fa38c5ba678f083c6d8788de9a3bf306b
Evelin <szabadosevelin7@gmail.com>
2020-09-28 12:59:14 +0200

    Implement two grid layout base for dashboard

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,9 +1,43 @@
 import React from 'react'
 
-export default function Dashboard(): JSX.Element {
-    return (
-        <div>
+import { Layout, List, Avatar } from 'antd';
+import { Row, Col } from 'antd';
 
-        </div>
+
+export default function Dashboard() {
+
+    const data = [
+        {
+            friend: 'Tamás Sallai',
+            displayMessage: 'How are you?'
+        },
+    ];
+
+    return (
+        <Layout style={{ height: '100vh' }}>
+            <div style={{ margin: '2rem auto', width: '80%', height: '80vh', backgroundColor: 'white' }}>
+                <Row>
+                    <Col span={6} style={{ padding: '1rem' }}>
+                        <List
+                            itemLayout="horizontal"
+                            dataSource={data}
+                            renderItem={item => (
+                                <List.Item>
+                                    <List.Item.Meta
+                                        avatar={<Avatar size={40}>{item.friend[0]}</Avatar>}
+                                        title={item.friend}
+                                        description={item.displayMessage}
+                                    />
+                                </List.Item>
+                            )}
+                        />
+                    </Col>
+                    <Col span={12}>ChatView</Col>
+                </Row>
+            </div>
+        </Layout>
     )
 }
+
+
+
