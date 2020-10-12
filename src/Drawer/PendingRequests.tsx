33a6baa135611486be8faa64cadbd36f86b60c5d
Evelin <szabadosevelin7@gmail.com>
2020-10-12 14:47:26 +0200

    Display pending requests

--- src/Drawer/PendingRequests.tsx
+++ src/Drawer/PendingRequests.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Avatar, List, Badge, Tag } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
 
 export default function PendingRequests() {
+
+    const data = [
+        {
+            displayName: 'Cecília Faragó',
+            email: 'cecilia@gmail.com'
+        }
+    ]
     return (
-        <div>
-            No pending friend requests
-        </div>
+        <List
+            style={{ marginTop: '1rem' }}
+            itemLayout="horizontal"
+            dataSource={data}
+            renderItem={friend => (
+                <List.Item actions={
+                    [
+                        <Tag style={{ cursor: 'pointer' }} icon={<DeleteOutlined />} color="error">Withdraw</Tag>
+                    ]
+                }>
+                    <List.Item.Meta
+                        avatar={<Badge offset={[0, 30]} status="success"><Avatar>{friend.displayName?.slice(0, 1)}</Avatar></Badge>}
+                        title={friend.displayName}
+                        description={friend.email}
+
+                    />
+                </List.Item>
+
+            )}
+        />
     )
 }
