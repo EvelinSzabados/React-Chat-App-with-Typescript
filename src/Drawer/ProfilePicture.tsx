0de47772825d3f34a79fb444d68cc5b737a11965
Evelin <szabadosevelin7@gmail.com>
2020-10-19 13:51:16 +0200

    Implement crop preview for profile pic upload

--- src/Drawer/ProfilePicture.tsx
+++ src/Drawer/ProfilePicture.tsx
@@ -1,43 +1,56 @@
 import React, { useState } from 'react'
 import { Avatar, Button, Popover, Upload, message } from 'antd';
 import { UserOutlined, UploadOutlined } from '@ant-design/icons';
+import ImgCrop from 'antd-img-crop';
+
 
 export default function ProfilePicture() {
     const [profilePicUrl, setProfilePicUrl] = useState('')
+
     const props = {
         name: 'file',
         action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
         headers: {
             authorization: 'authorization-text',
         },
-        onChange(info: any) {
-            if (info.file.status !== 'uploading') {
-                console.log(info.file, info.fileList);
+        onChange(fileObj: any) {
+
+            if (fileObj.file.status === 'done') {
+                message.success(`${fileObj.file.name} file uploaded successfully`);
+
+                setProfilePicUrl(fileObj.file.response.url)
+
+            } else if (fileObj.file.status === 'error') {
+                message.error(`${fileObj.file.name} file upload failed.`);
             }
-            if (info.file.status === 'done') {
-                message.success(`${info.file.name} file uploaded successfully`);
-                setProfilePicUrl(info.file.response.url)
-            } else if (info.file.status === 'error') {
-                message.error(`${info.file.name} file upload failed.`);
+
+        },
+        onPreview: async (file: any) => {
+            let src = file.url;
+            if (!src) {
+                src = await new Promise(resolve => {
+                    const reader = new FileReader();
+                    reader.readAsDataURL(file.originFileObj);
+                    reader.onload = () => resolve(reader.result);
+                });
             }
+            const image = new Image();
+            image.src = src;
+            const imgWindow = window.open(src);
+            imgWindow?.document.write(image.outerHTML);
         },
 
-        progress: {
-            strokeColor: {
-                '0%': '#108ee9',
-                '100%': '#87d068',
-            },
-            strokeWidth: 3,
-            format: (percent: any) => `${parseFloat(percent.toFixed(2))}%`,
-        },
     };
     return (
         <Popover placement="left" content={
-            <Upload showUploadList={false} {...props}>
-                <Button style={{ width: '50%', margin: '0 auto', border: 'none' }} icon={<UploadOutlined />} size='middle'>
-                    Change profile picture
+            <ImgCrop rotate>
+                <Upload showUploadList={false} {...props}>
+
+                    <Button style={{ width: '50%', margin: '0 auto', border: 'none' }} icon={<UploadOutlined />} size='middle'>
+                        Change profile picture
             </Button>
-            </Upload>
+                </Upload>
+            </ImgCrop>
         }>
 
             <Avatar size={64} icon={<UserOutlined />} src={profilePicUrl} style={{ margin: '0 auto', cursor: 'pointer' }} />
